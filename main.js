6a43163f9d85a26fcd1da87193bb5c446dc42ece
mikejzx <skec@protonmail.ch>
2019-11-29 22:28:01 +1100

    Webpage updated.

--- main.js
+++
@@ -1,9 +0,0 @@
-
-// Adjust parallax img based off scroll amount.
-function scrolled_callback() {
-    var scroll_amount = window.pageYOffset / 2;
-    var y = "calc(60% + " + scroll_amount + "px)";
-
-    document.getElementById("bg-main").setAttribute("style", 
-        "background-position: 50% " + y + " !important;");
-}
